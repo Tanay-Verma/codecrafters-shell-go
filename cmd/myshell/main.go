f5ebcc8eee516e550e41e9bf75dc45b0b7beced6
Tanay Verma <tanay.verma98@gmail.com>
2024-06-01 22:16:45 +0530

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -25,13 +25,19 @@ func repl() {
 		// removing the "/n" from the end of the command and storing the formatted string
 
 		inputArr := strings.Split(input, " ")
+		command := inputArr[0]
+		args := inputArr[1:]
 
-		if inputArr[0] == "exit" && len(inputArr) == 2 {
-			if inputArr[1] == "0" {
+		if command == "exit" && len(args) == 1 {
+			if args[0] == "0" {
 				os.Exit(0)
 			}
 		}
-		op := fmt.Sprintf("%s: command not found", inputArr[0][:len(inputArr[0])])
+		if command == "echo" {
+			fmt.Fprintln(os.Stdout, strings.Join(args, " "))
+			return;
+		}
+		op := fmt.Sprintf("%s: command not found", command)
 
 		fmt.Fprintln(os.Stdout, op)
 	}
