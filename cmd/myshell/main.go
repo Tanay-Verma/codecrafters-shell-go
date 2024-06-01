1b8358528a67d77b4ce327ff0de51b1e94548dd4
Tanay Verma <tanay.verma98@gmail.com>
2024-06-01 21:17:43 +0530

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,11 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	for {
-		repl();
+		repl()
 	}
 }
 
@@ -16,12 +17,22 @@ func repl() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	input, err := (bufio.NewReader(os.Stdin).ReadString('\n'))
+	input = strings.Trim(input, "\n")
 	if err != nil {
 		fmt.Fprintln(os.Stdout, err)
 	} else {
 		// removing the "/n" from the end of the command and storing the formatted string
-		op := fmt.Sprintf("%s: command not found", command[:len(command)-1])
+
+		inputArr := strings.Split(input, " ")
+
+		if inputArr[0] == "exit" && len(inputArr) == 2 {
+			if inputArr[1] == "0" {
+				os.Exit(0)
+			}
+		}
+		op := fmt.Sprintf("%s: command not found", inputArr[0][:len(inputArr[0])])
+
 		fmt.Fprintln(os.Stdout, op)
 	}
 }
