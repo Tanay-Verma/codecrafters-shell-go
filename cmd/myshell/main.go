8f2306c326fda201846a10feb96fe05fa3219bdc
Tanay Verma <tanay.verma98@gmail.com>
2024-06-01 19:12:35 +0530

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -11,10 +11,11 @@ func main() {
 
 	// Wait for user input
 	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err == nil {
+	if err != nil {
+		fmt.Fprintln(os.Stdout, err)
+	} else {
+		// removing the "/n" from the end of the command and storing the formatted string
 		op := fmt.Sprintf("%s: command not found", command[:len(command)-1])
 		fmt.Fprintln(os.Stdout, op)
-	} else {
-		fmt.Fprintln(os.Stdout, err)
 	}
 }
